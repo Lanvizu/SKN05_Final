4ca8467811abcdc82f1b7691c7798749126c0c54
Lanvizu <ghzm888@gmail.com>
2025-01-03 12:39:01 +0900

    test commit

--- backend/pjt_final/wsgi.py
+++ backend/pjt_final/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for pjt_final project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pjt_final.settings')
 
 application = get_wsgi_application()
