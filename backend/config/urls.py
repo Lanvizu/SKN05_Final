2a87715e8a0d603bf67113fca5a360d9bfe5d0cf
Lanvizu <ghzm888@gmail.com>
2025-01-14 14:21:51 +0900

    feat: BE 모든 URL 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 user = [
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('dj_rest_auth.urls')),
-    path("accounts/register/", include("dj_rest_auth.registration.urls")),
-    path('accounts/', include('allauth.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/accounts/', include('dj_rest_auth.urls')),
+    path("api/accounts/register/", include("dj_rest_auth.registration.urls")),
+    path('api/accounts/', include('allauth.urls')),
 ]
 
 urlpatterns.extend(user)
