284d9fc95c6f46e391d29d9c90a33370c717a610
Lanvizu <ghzm888@gmail.com>
2025-02-11 17:37:06 +0900

    feat: AWS 환경 변경

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,7 +6,7 @@ from .utils import Initialize_env_variables
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR.parent / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
